5503112f3990ef2bc41af91fca6c4c328e756de1
nramin <nramin@uw.edu>
2014-04-23 19:52:37 -0700

    Put in files

---
+++ WebService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Newtonsoft.Json;
+using System.Web.Script.Services;
+using System.Web.Script.Serialization;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public string GetNumbersThatStartsWith(int n)
+    {
+        Random gen = new Random();
+        List<int> RandomNumbers = new List<int>();
+        for (int i = n; i < 10000; i++)
+        {
+            RandomNumbers.Add(gen.Next(int.MaxValue));
+        }
+        return new JavaScriptSerializer().Serialize(RandomNumbers);
+    }
+    
+}
